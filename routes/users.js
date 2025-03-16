1a6ba0f4b773914a2798aab2a8447b2d17143ef1
Adicondro Yusuf <adicondro.yusuf@gmail.com>
2025-03-16 21:00:22 +0700

    Learn REST API

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,20 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
+uuidv4();
 
 const router = express.Router();
 
 const users = [
-  {
-    name: "John",
-    lastName: "Doe",
-    age: 25,
-  },
-  {
-    firstname: "Jane",
-    lastName: "Doe",
-    age: 24,
-  },
+  // {
+  //   firstname: "John",
+  //   lastName: "Doe",
+  //   age: 25,
+  // },
+  // {
+  //   firstname: "Jane",
+  //   lastName: "Doe",
+  //   age: 24,
+  // },
 ];
 
 // Alreadu in users route
@@ -24,9 +26,22 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   const user = req.body;
 
-  users.push(user);
+  const userId = uuidv4();
+
+  users.push({
+    ...user,
+    id: userId,
+  });
 
   res.send(`User with the name ${user.firstname} added to the database!`);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
 });
 
 export default router;
